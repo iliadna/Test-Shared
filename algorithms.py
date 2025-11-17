870cda2588f8e9196cc3a3fa027d3021164aa692
Inna Dudinets <iliadna@gmail.com>
2025-11-17 02:31:33 +0100

    Add algorithms

---
+++ algorithms.py
@@ -0,0 +1,24 @@
+def binary_search(arr: list[int], target: int) -> int:
+    low, high = 0, len(arr) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return -1
+
+
+def bubble_sort(arr: list[int]) -> list[int]:
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr
+
+
+def unique_items(arr: list[int]) -> list[int]:
+    return list(set(arr))
